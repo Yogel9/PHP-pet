698b92e307993de236db7c4c86cc6a388f5811ec
Антон Довиденков <79474789+Yogel9@users.noreply.github.com>
2021-08-06 15:35:43 +0400

    Добавлен файл .htaccess
    AddDefaultCharset UTF-8
    Установка стандартной кодировки
    
    RewriteRule ^about?$ about.php [NC, L]
    замена адресации about.php на about
    [NC, L] - регистр неважен

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8"> <!-- кодировка -->
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"><!-- для мобильных -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"> <!-- bootstrap -->
+	<title>
+		Eror 404
+	</title>
+</head>
+<body>
+
+	
+<?php require "blocks/header.php" ?>
+
+<div class="container mt-5">
+	<h3 class="mb-5"> 
+   Eror404
+	</h3>
+</div>
+	
+
+
+<?php require "blocks/footer.php" ?>
+
+
+</body>
+</html>
